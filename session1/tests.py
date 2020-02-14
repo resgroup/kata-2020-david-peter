0f70e42b306e125fdebc0da729f58d71748c6770
dstevens <david.stevens@res-group.com>
2020-02-14 11:30:07 +0100

    Added tests on second row

--- session1/tests.py
+++ session1/tests.py
@@ -11,5 +11,25 @@ class KataTests(unittest.TestCase):
         for seconds in range(1, 60, 2):
             self.assertEqual('O', kata.first_row(seconds))
 
+    def test_second_row_less_than_5(self):
+        for hours in range(0,5):
+            self.assertEqual('OOOO', kata.second_row(hours))
+
+    def test_second_row_between_5_and_9(self):
+        for hours in range(5,10):
+            self.assertEqual('ROOO', kata.second_row(hours))
+
+    def test_second_row_between_10_and_14(self):
+        for hours in range(10,15):
+            self.assertEqual('RROO', kata.second_row(hours))
+
+    def test_second_row_between_15_and_19(self):
+        for hours in range(15,20):
+            self.assertEqual('RRRO', kata.second_row(hours))
+
+    def test_second_row_between_20_and_23(self):
+        for hours in range(20,24):
+            self.assertEqual('RRRR', kata.second_row(hours))
+
 if __name__ == '__main__':
     unittest.main()
